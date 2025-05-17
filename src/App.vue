4a8826eaa51c6b6b2ecc3790509d23f623ad6056
auauauau <auauauau@gmail.com>
2025-05-16 22:36:40 -0300

    Craição tela Mercado Livre

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import Navbar from "./components/NavbarComponent.vue";
+import Footer from "./components/FooterComponent.vue";
+
+</script>
+
+<template>
+  <Navbar />
+  <router-view> </router-view>
+  <div class="mflex flex-col min-h-screen px-4 py-6">
+    </div>
+    <Footer />
+</template>
