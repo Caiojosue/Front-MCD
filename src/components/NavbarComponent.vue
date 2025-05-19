0b837dd040bbc61e68b10ef284b37240ff34a675
auauauau <auauauau@gmail.com>
2025-05-19 19:34:39 -0300

    atualização front

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -7,6 +7,10 @@
       </div>
 
       <nav class="space-y-2">
+        <form action="" class="justify-between ">
+          <input type="text" placeholder="Pesquisar" class="p-1 rounded-tl-lg rounded-br-lg">
+          <button type="submit" class="bg-blue-400 p-1 rounded-lg">Buscar</button>
+        </form>
         <router-link
           to="/about"
           class="sidebar-link flex items-center space-x-3 p-3 rounded-lg bg-blue-50 text-blue-600"
@@ -29,7 +33,7 @@
         </router-link>
         <a
           href=""
-          class="sidebar-link flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 text-gray-600"
+          class="sidebar-link flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 text-gray-600 hover:text-blue-600"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +54,7 @@
         </a>
         <a
           href=""
-          class="sidebar-link flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 text-gray-600"
+          class="sidebar-link flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 text-gray-600 hover:text-blue-600"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -69,27 +73,7 @@
 
           <span>Dashboard</span>
         </a>
-        <a
-          href=""
-          class="sidebar-link flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 text-gray-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-            />
-          </svg>
-
-          <span>Relatórios</span>
-        </a>
+       
       </nav>
     </div>
   </body>
