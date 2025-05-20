c1ffa31f6fc3d71cc5d559a1fa1c14674b2588f6
auauauau <auauauau@gmail.com>
2025-05-19 21:46:07 -0300

    atualizção 2

---
+++ src/views/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex justify-center w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table>
+      <tr>
+        <th scope="col" class="px-10 py-5">ID</th>
+        <th scope="col" class="px-10 py-5">Nome</th>
+        <th scope="col" class="px-10 py-5">Preço</th>
+        <th scope="col" class="px-10 py-5">Categoria</th>
+        <th scope="col" class="px-10 py-5">Quantidade Venda</th>
+      </tr> 
+    </table>
+  </div>
+</template>
